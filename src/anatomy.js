db10a544cf65cd2278716bdc90edefb6b28fb00b
bsed <bsedscience4a@gmail.com>
2021-11-22 21:02:10 +0800

    Update anatomy.js

--- src/anatomy.js
+++ src/anatomy.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ThreeJsCanvas from "./components/threejs_canvas";
+import 'react-perfect-scrollbar/dist/css/styles.css';
+import PerfectScrollbar from 'react-perfect-scrollbar'
 // import PropTypes from "prop-types";
 export default function Anatomy() {
   return (
+    <PerfectScrollbar>
     <div className={"common-container anatomy-container col-md-12"}>
       <ThreeJsCanvas  orientation="landscape"/>
     </div>
+    </PerfectScrollbar>
   )
 }
 Anatomy.propTypes = {};
